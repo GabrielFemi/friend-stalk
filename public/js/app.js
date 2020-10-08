16f3a929d89787b61c8385fc8a07b12a0258abff
Gabriel Femi <gabrielfemi799@gmail.com>
2020-10-08 08:53:42 +0100

    Change placeholder in stalk form

--- public/js/app.js
+++ public/js/app.js
@@ -28139,7 +28139,7 @@ var render = function() {
                   "py-5 bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal",
                 attrs: {
                   type: "text",
-                  placeholder: "TolulopeFaleye",
+                  placeholder: "@TolulopeFaleye",
                   required: ""
                 },
                 domProps: { value: _vm.form.stalkee_name },
